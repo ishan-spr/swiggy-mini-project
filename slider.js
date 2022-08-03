9a1ed52620d7848578abbee4756d3b9c57c30669
ishan-spr <ishan.chaudhary@sprinklr.com>
2022-08-03 15:40:36 +0530

    Slider direction bug fix

--- slider.js
+++ slider.js
@@ -9,40 +9,33 @@
 
         let leftBtn = document.querySelector('.carousel-btn[data-direction="left"]')
         let rightBtn = document.querySelector('.carousel-btn[data-direction="right"]')
-
         let carouselWrapper = document.querySelector('.carousel-wrapper')
-        leftBtn.addEventListener('click', () => {
-            
-            let carouselOffsets = getOffSets(carouselWrapper)
-            let carouselItems = document.querySelectorAll('.carousel-card')
 
-            if(carouselOffsets.right >= getOffSets(carouselItems[carouselItems.length -1]).right){
+        leftBtn.addEventListener('click', () => {  
+            let carouselItems = document.querySelectorAll('.carousel-card')
+            let width = carouselItems[0].offsetWidth + 23;
+            let carouselOffsets = getOffSets(carouselWrapper)
+            if( Math.ceil(carouselOffsets.left) + 10 <= Math.ceil(getOffSets(carouselItems[0]).left)){
                 leftBtn.style.visibility = 'hidden'   
             }else{
                 rightBtn.style.visibility='visible'
             }
-         
-            let width = carouselItems[0].offsetWidth + 23;
-            currOffset = currOffset - width
-            for(let item of carouselItems){
-                item.style.transform = `translateX(${currOffset}px)`
-                item.style.transitionDuration = "1s"
-            }
-        })
-        rightBtn.addEventListener('click', () => {
 
+            carouselWrapper.scrollBy(-width,0)
+        })
+
+        rightBtn.addEventListener('click', () => {
             let carouselItems = document.querySelectorAll('.carousel-card')
-            let width = carouselItems[0].offsetWidth + 23;
             let carouselOffsets = getOffSets(carouselWrapper)
-            if(getOffSets(carouselItems[1]).right >= carouselOffsets.right){
-                rightBtn.style.visibility = 'hidden'               
+            
+            if(Math.ceil(carouselOffsets.right) - 10 >= Math.ceil(getOffSets(carouselItems[carouselItems.length -1]).right)){
+                rightBtn.style.visibility = 'hidden'   
             }else{
                 leftBtn.style.visibility='visible'
             }
-            currOffset = currOffset + width
-            for(let item of carouselItems){
-                item.style.transform = `translateX(${currOffset}px)`
-            }
+         
+            let width = carouselItems[0].offsetWidth + 23;
+            carouselWrapper.scrollBy(width,0)
         })
     }
 )()
